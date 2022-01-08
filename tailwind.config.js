6438707c20084f129877c023ddac9c558903e0c4
Arnaud Ligny <arnaud@ligny.org>
2022-01-08 03:55:25 +0100

    New theme powered by Tailwind (#17)

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    content: [
+        './layouts/**/*.html.twig',
+        './themes/**/layouts/**/*.html.twig',
+    ],
+    darkMode: 'media',
+    theme: {
+        container: {
+            screens: {
+                sm: '100%',
+                md: '100%',
+                lg: '1024px',
+                xl: '1024px',
+            }
+        }
+    },
+    variants: {
+        typography: ['dark'],
+    },
+    plugins: [
+        require('@tailwindcss/typography')({
+            className: 'markdown',
+        }),
+    ],
+}
