2b66aa1a60e15a5d526846d6e02ed9cecae9be70
Arnaud Ligny <arnaud@ligny.org>
2022-01-08 23:43:08 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 module.exports = {
-    content: [
-        './layouts/**/*.html.twig',
-        './themes/**/layouts/**/*.html.twig',
-    ],
-    darkMode: 'media',
-    theme: {
-        container: {
-            screens: {
-                sm: '100%',
-                md: '100%',
-                lg: '1024px',
-                xl: '1024px',
-            }
-        }
+  content: [
+    './layouts/**/*.html.twig',
+    './themes/**/layouts/**/*.html.twig',
+  ],
+  darkMode: 'media',
+  theme: {
+    container: {
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1024px',
+        xl: '1024px',
+      }
     },
-    variants: {
-        typography: ['dark'],
-    },
-    plugins: [
-        require('@tailwindcss/typography')({
-            className: 'markdown',
-        }),
-    ],
+  },
+  plugins: [
+    require('@tailwindcss/typography')({
+      className: 'markdown',
+    }),
+  ],
 }
